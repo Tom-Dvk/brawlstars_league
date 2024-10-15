3caa2db6eb88efc12c519d4730657541002e6a5e
Tom-Dvk <159052433+Tom-Dvk@users.noreply.github.com>
2024-10-15 09:01:49 +0200

    first changes on brawler, team and league .py

--- team.py
+++ team.py
@@ -1,1 +1,16 @@
-# File for the team class
+#####################################
+##  This script define the teams   ##
+#####################################
+
+class Team:
+    def __init__(self, name):
+
+        self.name = name # A team has a name
+        self.brawlers = [] # And has brawlers
+        self.max_brawlers = 3 # But a team cannot exceed 3 brawlers
+    
+    def add_player(self, brawler):
+        ''' 
+        Method to add a brawler in a Team
+        '''
+        self.brawlers.append(brawler) 
