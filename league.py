96b608f049aebe0bfb82ac9c90e227d56f07d167
n-pizzetta <154426149+n-pizzetta@users.noreply.github.com>
2024-10-20 11:25:56 +0200

    Started a new League class.

--- league.py
+++ league.py
@@ -1,34 +1,16 @@
-######################################
-##  This script define the league   ##
-######################################
-
-from typing import List, Union, Dict
-
-# First Class define a game
-class Game:
-    def __init__(self, team1: List[str], team2: List[str]) -> None:
-        self.team1 = team1
-        self.team2 = team2
-    
-    def play_game(self, teams: Dict{List[str]}, mode={}) -> Union[str, None]: 
-        '''
-        The method to play the game and return a winner 
-        '''
-
-        ## we have to define how a match is win for a team. Using specificities of brawlers and teams.
-        return None
-    
-    def get_results(self, play_game):
-        score = play_game(self.)
-        print()
-    
-class Tournament:
+class League:
     def __init__(self, teams):
+        self.teams = teams
+        self.matches = []
+        self.results = {}
 
-        self.teams = teams # Contains all the teams that are playing the tournament
+    def schedule_match(self, team1, team2, grid):
+        """Schedule a match between two teams."""
+        match = Match(team1, team2, grid)
+        self.matches.append(match)
+        print(f"Scheduled match: {team1.name} vs {team2.name}")
 
-    def simulate(self):
-        '''
-        This function simulate the whole tournament, and return the team of the winners
-        '''
-        return None
+    def play_matches(self):
+        """Play all scheduled matches."""
+        for match in self.matches:
+            match.play_match()
